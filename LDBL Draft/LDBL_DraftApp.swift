a8d0e84625d26d2d354dff23a1df5ddf214dd5c5
Brennan Dumm <bjdumm23@gmail.com>
2026-08-14 00:40:16 -0400

    Initial Commit

---
+++ LDBL Draft/LDBL_DraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LDBL_DraftApp.swift
+//  LDBL Draft
+//
+//  Created by Brennan Dumm on 8/14/26.
+//
+
+import SwiftUI
+
+@main
+struct LDBL_DraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
